d455bd01be5d5956be6f5d2623e120092636634f
Anton Tursenev <anton@rikishi.info>
2020-06-12 18:13:24 +0700

    Add user chart options

--- packages/pink-unicorn-utils/src/index.js
+++ packages/pink-unicorn-utils/src/index.js
@@ -1,11 +1,4 @@
-// import ApiRequest from './ApiRequest'
-// import UserApiKeyRequest from './UserApiKeyRequest'
-// import Deferred from './Deferred'
-
-// export default {
-//   ApiRequest,
-// }
-
 export { default as ApiRequest } from './ApiRequest'
 export { default as UserApiKeyRequest } from './UserApiKeyRequest'
+export { default as UserApiKeyChart } from './UserApiKeyChart'
 export { default as Deferred } from './Deferred'
